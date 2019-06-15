90ff036f56a053df081bc9a7672fde97e2dd803b
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-15 11:18:08 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/fragmentorientation2802/FragmentList.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragmentorientation2802/FragmentList.java
@@ -13,13 +13,10 @@ import java.util.ArrayList;
 public class FragmentList extends ListFragment {
 
     View view;
-
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_list,container,false);
-
         return view;
     }
 
