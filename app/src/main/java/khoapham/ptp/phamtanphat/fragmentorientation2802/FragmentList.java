016fe4b6b875512b7d76b3392dddd63f22e8be38
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-15 11:27:01 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/fragmentorientation2802/FragmentList.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/fragmentorientation2802/FragmentList.java
@@ -13,10 +13,22 @@ import java.util.ArrayList;
 public class FragmentList extends ListFragment {
 
     View view;
+    ArrayList<String> mangten = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_list,container,false);
+
+
+        mangten.add("Nguyen Van A");
+        mangten.add("Nguyen Van B");
+        mangten.add("Nguyen Van C");
+        mangten.add("Nguyen Van D");
+        mangten.add("Nguyen Van E");
+        arrayAdapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,mangten);
+        setListAdapter(arrayAdapter);
         return view;
     }
 
